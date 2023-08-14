427a1f4ff54d9095a638f3e64284580217202c87
Putro Satrio Wibowo <riomabox@gmail.com>
2023-08-14 15:37:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# aghniputro-wedding
+ Website Undangan Pernikahan Aghni & Putro
+
+ Technologies:
+ * Tailwindcss 3.3.3
+ * PostCSS 8.4.26
+ * AutoPrefixer 10.4.14
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: "Blackout",
+        byp: "Broted-young-plant",
+        bypscript: "Broted-young-plant-script",
+        qslight: "Quiche-sans-light",
+        lato: "Lato",
+        poppins: "Poppins",
+      },
+    },
+  },
+  plugins: [],
+};
