c0e431485d5eb25e28126d882542848a16d3d635
Putro Satrio Wibowo <riomabox@gmail.com>
2023-08-14 21:39:35 +0700

    chore: Menambah color pada design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,33 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        primary: "Blackout",
-        byp: "Broted-young-plant",
-        bypscript: "Broted-young-plant-script",
-        qslight: "Quiche-sans-light",
-        lato: "Lato",
-        poppins: "Poppins",
+        byp: ["Broted-young-plant"],
+        bypscript: ["Broted-young-plant-script"],
+        qslight: ["Quiche-sans-light"],
+        lato: ["Lato"],
       },
+      screens: {
+        tablet: "960px",
+        desktop: "1280px",
+      },
+      colors: {
+        primary: {
+          OlderSage: "#819D6D",
+          Olive: "#46553B",
+        },
+        secondary: {
+          "Brl-01": "#F7E4BD",
+          "Brl-02": "#E7CD94",
+          "Brl-03": "#B49349",
+          "Brl-04": "#5E460F",
+        },
+        neutral: {
+          White: "#FFF8EE",
+          Black: "#36290B",
+          Grey: "#B8B2A4",
+        },
+      },
+      fontSize: {},
     },
   },
   plugins: [],
