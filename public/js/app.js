05f4cf8dc8dd5bef1b31dba96cac42b88b0caf6d
Putro Satrio Wibowo <riomabox@gmail.com>
2023-08-29 19:47:17 +0700

    chore: Menambah googlemap api

--- public/js/app.js
+++ public/js/app.js
@@ -189,7 +189,6 @@ window.addEventListener(
       document.getElementById("tujuan-tamu").remove();
     } else {
       let namaTamu = document.getElementById("nama-tamu");
-      console.log(name);
       namaTamu.innerText = escapeHtml(name);
       // document.getElementById("formnama").value = name;
     }
@@ -203,3 +202,29 @@ window.addEventListener(
   },
   false,
 );
+
+async function initMap() {
+  // The location of Uluru
+  let map;
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
